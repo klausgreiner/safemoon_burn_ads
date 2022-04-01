9ac4a93f2f739eb770d354321e5fc3e66fcb631a
Klaus Greiner <klausgreinerfiorini@gmail.com>
2022-03-31 22:56:01 -0300

    [ADD] firebase & change project name

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'src/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   _initGoogleMobileAds();
   runApp(MyApp());
 }
 
 Future<InitializationStatus> _initGoogleMobileAds() {
-  WidgetsFlutterBinding.ensureInitialized();
   return MobileAds.instance.initialize();
 }
